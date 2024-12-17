33735649cef88dada3e47682f33f02439612e4cd
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2024-12-17 17:04:12 +0800

    Update neo4j.py

--- neo4j.py
+++ neo4j.py
@@ -1,29 +1,35 @@
 from pyspark.sql import SparkSession
 from UtilsNeo4J import setup_neo4j_driver, insert_into_neo4j, populate_database
 import redis
+
 # PySpark setup
 spark = SparkSession.builder \
     .appName("Populate Neo4j") \
     .getOrCreate()
 
-# Load data
-word_details_csv_cleaned = spark.read.csv("assignData/word_details_csv_cleaned", header=True)
+# Load data from the cleaned CSV file
+word_details_csv_cleaned = spark.read.csv("assignData/word_details_csv_cleaned_test", header=True)
+# Convert the Spark DataFrame to a list of Rows for easier processing
 data = word_details_csv_cleaned.collect()
-print(f"Output of word_details_csv_cleaned.show(20): {word_details_csv_cleaned.show(20)}")
+print(f"data: {word_details_csv_cleaned.show(20)}")
 
-# Setup Neo4j and Redis
+
+
+
+# Setup Neo4j driver
 driver = setup_neo4j_driver(
-    uri="neo4j+s://f2d488e8.av.neo4j.io",
+    uri="neo4j+s://abc.databases.neo4j.io",
     user="neo4j",
-    password="va"  # Remember to replace!
+    password="abc"  # Remember to replace with your actual password!
 )
 
+# Setup Redis client
 redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 
-# populate_database with data
+# Populate the Neo4j database with the data
 populate_database(driver, redis_client, data)
 
-# Query Neo4j for total nodes
+# Query Neo4j to verify the number of nodes created
 with driver.session() as session:
     query = "MATCH (n) RETURN count(n) as totalNodes"
     result = session.run(query)
