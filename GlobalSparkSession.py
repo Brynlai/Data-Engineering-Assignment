2b7d015a3f217f7cc6314fa55f83717f4093d85a
PinkWhite <134790534+Brynlai@users.noreply.github.com>
2024-12-18 11:54:16 +0800

    Update GlobalSparkSession.py

--- GlobalSparkSession.py
+++ GlobalSparkSession.py
@@ -2,6 +2,15 @@ from pyspark.sql import SparkSession
 
 # Initialize SparkSession
 def global_spark_session():
+    """
+    Creates or retrieves a SparkSession.
+
+    This function initializes a SparkSession if one doesn't already exist, or retrieves the existing one.
+    It's designed to be called globally to ensure only one SparkSession is active in the application.
+
+    Returns:
+        SparkSession: The active SparkSession instance.
+    """
     return SparkSession.builder \
         .appName("data-engineering-project") \
         .getOrCreate()
