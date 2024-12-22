8f2ced162d5112bc95700ec44ead9dbc17172ab2
Xavier <xaviernky-wm22@student.tarc.edu.my>
2024-12-22 19:57:11 +0800

    Update LexiconSizeAndCoverage.py

--- LexiconSizeAndCoverage.py
+++ LexiconSizeAndCoverage.py
@@ -1,1 +1,13 @@
-
+"""
+Author  : Xavier Ngow Kar Yuen
+Date    : 22/12/2024
+"""
+from pyspark.sql import Row
+from pyspark.sql.functions import col, countDistinct
+from UtilsRedis import Redis_Utilities
+from neo4j import GraphDatabase
+from GlobalSparkSession import GlobalSparkSession
+import redis
+import matplotlib.pyplot as plt
+# Initialize Spark session
+spark = GlobalSparkSession.get_instance()
